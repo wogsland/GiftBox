a18b79b16926e281cc7b27395cd1d772aa2d112c
John Junsong Zhang <junsong.zhang@vanderbilt.edu>
2014-06-11 21:24:54 -0500

    refresh page after log out

--- js/account.js
+++ js/account.js
@@ -115,6 +115,8 @@ function logout() {
     	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
     	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
+    document.location.href = '/';
+
 }
 
 window.fbAsyncInit = function() {
