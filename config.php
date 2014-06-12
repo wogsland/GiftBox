f2fe0cf6f7c85acc3845709e26aa9d6cb62074ba
John Hall <john_hall@corridor-inc.com>
2014-06-11 22:27:08 -0400

    Updated config and database connection to accomodate both development and test servers

--- config.php
+++ config.php
@@ -1,16 +1,13 @@
 <?php
-$server = "localhost";
+if ($_SERVER['SERVER_NAME'] == "giftbox.com") {
+	$server = "localhost";
+} else if ($_SERVER['SERVER_NAME'] == "stone-timing-557.appspot.com") {
+	$server = "173.194.244.175";
+}
 $user = "giftbox";
 $password = "giftbox";
-/*
-$server = "173.194.244.175";
-$user = "giftbox";
-$password = "giftbox";
-*/
-
 $database = "giftbox";
 $app_name = "Giftbox";
-$app_root = "http://localhost/giftbox";
+$app_root = "http://giftbox.com";
 $sender_email = "admin@giftbox.com";
-
 ?>
