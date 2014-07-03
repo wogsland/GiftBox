617c3cd942f4d11a9942de47363443528b7f1120
John Hall <john_hall@corridor-inc.com>
2014-07-02 23:40:28 -0400

    google mail test

---
+++ mail_test.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'config.php';
+use \google\appengine\api\mail\Message;
+
+try
+{
+	$email_message = " To activate your Giftbox account, please click on this link:\n\n";
+	sendMail('john_hall@corridor-inc.com', 'Giftbox Registration Confirmation', $email_message, $sender_email);
+} catch (InvalidArgumentException $e) {
+	echo $e.getMessage();
+}
+?>
