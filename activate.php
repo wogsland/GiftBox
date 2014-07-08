780b5293345b7044640efc80e4263c562dca085d
John Hall <john_hall@corridor-inc.com>
2014-07-08 13:08:38 -0400

    Switch to using mail wrapper

--- activate.php
+++ activate.php
@@ -4,15 +4,14 @@ include_once 'util.php';
 include_once 'database.php';
 include_once 'eventLogger.class.php';
 
-if (isset($_GET['email']) && isset($_GET['key'])) {
-	$email = $_GET['email'];
+if (isset($_GET['uid']) && isset($_GET['key'])) {
+	$user_id = $_GET['uid'];
 	$key = $_GET['key'];
 	try {
-		$rows_affected = update("UPDATE user SET activation_key = NULL WHERE email_address = '$email' AND activation_key = '$key' LIMIT 1");
+		$rows_affected = update("UPDATE user SET activation_key = NULL WHERE id = $user_id AND activation_key = '$key' LIMIT 1");
 		if ($rows_affected != 1) {
 			throw new Exception('Update failed');
 		}
-		$user_id = execute_query("SELECT id from user where email_address = '$email'")->fetch_object()->id;
 		$event = new eventLogger($user_id, ACTIVATE_ACCOUNT);
 		$event->log();
 		echo '<div>Your account is now active. You may now <a href="'.$app_root.'">Log in</a></div>';
