f80e4ea28e117fde8453f7d8b926b286575ae28c
Gary Mazzone Peters <gary.m.peters@vanderbilt.edu>
2014-11-25 13:29:30 -0600

    Started adding in privacy page and terms of service page, return button not working

---
+++ termsservice.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Giftbox - About</title>
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/create.css" />
+</head>
+<body>
+	<div id="content-wrapper">
+		<div class="header-wrapper" id="create-header-wrapper">
+			<header>
+				<h1>
+					<a id="create-home-icon" title="Return to the Homepage" href="<?php echo $app_root ?>">Giftbox</a>
+				</h1>
+				<nav id="create-top-nav">
+					<ul>
+						<li>
+							<a href="<?php echo $app_root ?>">Home</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+	</div>
+</body>
+</html>
