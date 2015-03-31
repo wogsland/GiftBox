e44963f414ec5aedd68297d9132b78d3ccf76afc
John Hall <john_hall@corridor-inc.com>
2015-03-31 11:13:53 -0400

    257-Partial Laravel integration

--- get_session_ajax.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once 'config.php';
-include_once 'util.php';
-
-_session_start();
-$_SESSION['app_root'] = $app_root;
-header('Content-Type: application/json');
-echo json_encode($_SESSION);
--- update_my_account_ajax.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-execute("UPDATE user set first_name = '".$_POST['first-name']."', last_name = '".$_POST['last-name']."', email_address = '".$_POST['email']."' WHERE id = ".$_SESSION['user_id']);
-$event = new eventLogger($_SESSION['user_id'], UPDATE_ACCOUNT_INFO);
-$event->log();
