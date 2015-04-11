1b581719c0f4a8f9b22a72819de611bb82b6a6d5
benstucki <benstucki@gmail.com>
2015-04-10 23:44:19 -0500

    added attachments feature. issue #299

--- get_token_ajax.php
+++ get_token_ajax.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['user_id'])) {
 		$sql = "SELECT * FROM giftbox where user_id = ".$_SESSION['user_id']." and id = ".$_GET['id'];
 		$response = execute_query($sql)->fetch_object();
 		$response->app_url = $_SESSION['app_url'];
-		
+
 		$response->bentos = array();
 		$sql = "SELECT * FROM bento WHERE giftbox_id = ".$_GET['id']." ORDER BY CAST(css_left AS SIGNED), CAST(css_top AS SIGNED)";
 		$results = execute_query($sql);
@@ -30,7 +30,13 @@ if (isset($_SESSION['user_id'])) {
 		while ($divider = $results->fetch_object()) {
 			$response->dividers[count($response->dividers)] = $divider;
 		}
-		
+
+		$sql = "SELECT * FROM attachment WHERE giftbox_id = {$_GET['id']} GROUP BY download_file_name";
+		$results = execute_query($sql);
+		while ($attachment = $results->fetch_object()) {
+			$response->attachments[count($response->attachments)] = $attachment;
+		}
+
 		header('Content-Type: application/json');
 		echo json_encode($response);
 	}
--- preview.php
+++ preview.php
@@ -1,5 +1,10 @@
 <?php
 	include_once 'Token.class.php';
+	include_once 'config.php';
+	use google\appengine\api\cloud_storage\CloudStorageTools;
+	if ($google_app_engine) {
+		include_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+	}
 
 	$token = new Token($_GET['id']);
 ?>
@@ -8,26 +13,41 @@
 <head>
 	<meta charset="utf-8" />
 	<title><?php echo $token->name ?></title>
+
 	<link rel="stylesheet" href="css/preview.css" />
 	<link rel="stylesheet" href="css/create_and_preview.css" />
-	<link rel="stylesheet" href="//vjs.zencdn.net/4.11/video-js.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!--<link rel="stylesheet" href="//vjs.zencdn.net/4.11/video-js.css">-->
 	<script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
-	<script src="//vjs.zencdn.net/4.11/video.js"></script>
+	<!--<script src="//vjs.zencdn.net/4.11/video.js"></script>-->
 	<script src="js/preview.js"></script>
 </head>
 <body>
-	<?php include_once("analyticstracking.php") ?>
-	<?php 
+	<?php /*include_once("analyticstracking.php")*/ ?>
+	<?php
 	echo '<div class="giftbox panel" id="flip-container">';
 	echo '<div class="front">';
 	echo $token->letter_text ? '<a class="flip-over flip-tab" id="view-letter" href="javascript:void(0);">View Letter</a>'.PHP_EOL : NULL;
-	
+
 	$token->render();
 
 	echo "</div>";
 	echo '<div class="back">';
 	echo $token->letter_text ? '<a class="flip-back flip-tab" id="close-letter" href="javascript:void(0);">View Token</a>'.PHP_EOL : NULL;
-	echo '<p id="letter-text">'.nl2br($token->letter_text).'</p>';
+
+	echo '<div id="letter-text">';
+	echo '<p>'.nl2br($token->letter_text).'</p>';
+	echo '<p id="letter-attachments">';
+	foreach ($token->attachments as $attachment) {
+		if ($google_app_engine) {
+			$image_path = CloudStorageTools::getPublicUrl($file_storage_path.$attachment->download_file_name, $use_https);
+		} else {
+			$image_path = $file_storage_path.$attachment->download_file_name;
+		}
+		echo '<a href="'.$image_path.'" target="_blank"><i class="fa fa-file fa-x2"></i> '.$attachment->file_name.'</a>';
+	}
+	echo '</p>';
+	echo '</div>';
 	echo "</div>";
 	echo "</div>";
 	?>
