56166ef373a71a7096052d7ad0b8838f6ab5e545
John Hall <john_hall@corridor-inc.com>
2015-05-01 17:45:24 -0400

    Show "View" link in preview for text OR attachments.

--- preview.php
+++ preview.php
@@ -47,7 +47,7 @@
 	<?php
 	echo '<div class="giftbox panel" id="flip-container">';
 	echo '<div class="front">';
-	echo $token->letter_text ? '<a class="flip-over flip-tab" id="view-letter" href="javascript:void(0);">View Letter</a>'.PHP_EOL : NULL;
+	echo ($token->letter_text || $token->attachments)  ? '<a class="flip-over flip-tab" id="view-letter" href="javascript:void(0);">View Letter</a>'.PHP_EOL : NULL;
 
 	$token->render();
 
