67266abef10bf2449389a0919b648de1a45da7be
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-06-10 19:58:29 -0500

    Loader working, background changed

--- preview.php
+++ preview.php
@@ -24,6 +24,15 @@
 	<script src="//vjs.zencdn.net/4.11/video.js"></script>
 	<script src="js/preview.js"></script>
 
+	<!-- CUSTOM STYLESHEETS -->
+	<link rel="stylesheet" href="css/styles.css">
+	<style>
+	body {
+		background-color: none;
+	}
+	</style>
+	<script src="js/custom.js"></script>
+
 	<!-- Favicon -->
 	<link rel="apple-touch-icon" sizes="57x57" href="assets/gt-favicons.ico/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="assets/gt-favicons.ico/apple-icon-60x60.png">
@@ -46,11 +55,13 @@
 </head>
 <body>
 	<?php /*include_once("analyticstracking.php")*/ ?>
+	<!-- =========================
+	     PRE LOADER       
+	============================== -->
+	<div class="preloader">
+	  <div class="status">&nbsp;</div>
+	</div>
 	<?php
-	// echo '<div class="preloader">';
-	// echo '<div class="status">&nbsp;</div>';
-	// echo '</div>';
-	// echo '<header></header>';
 	echo '<div class="giftbox panel" id="flip-container">';
 	echo '<div class="front">';
 	echo ($token->letter_text || $token->attachments)  ? '<a class="flip-over flip-tab" id="view-letter" href="javascript:void(0);">View Letter</a>'.PHP_EOL : NULL;
