fe1159a2db9e04f87f6b5d5a167c7caad4fa57b6
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-06-16 12:15:54 -0500

    small change to template 11

--- templates/preview-template-11.php
+++ templates/preview-template-11.php
@@ -1,13 +1,13 @@
 <div class="template" id="preview">
-	<div id="column-11-1" class="padded column height50 width100" <?php $this->columns['column-11-1']->width_percentage(); ?>>
-		<?php $this->bentos["bento-10-1"]->render(); ?>
+	<div id="column-11-1" class="padded column height50 width100" <?php $this->columns['column-11-1']->height_percentage(); ?>>
+		<?php $this->bentos["bento-11-1"]->render(); ?>
 	</div>
-	<div id="column-11-2" class="column height50 width100" <?php $this->columns['column-11-2']->width_percentage(); ?>>
-		<div id="column-11-3" class="padded column height100 width50" <?php $this->columns['column-11-3']->height_percentage(); ?>>
-			<?php $this->bentos["bento-10-2"]->render(); ?>
+	<div id="column-11-2" class="column height50 width100" <?php $this->columns['column-11-2']->height_percentage(); ?>>
+		<div id="column-11-3" class="padded column height100 width50" <?php $this->columns['column-11-3']->width_percentage(); ?>>
+			<?php $this->bentos["bento-11-2"]->render(); ?>
 		</div>
-		<div id="column-11-4" class="padded column height100 width50" <?php $this->columns['column-11-4']->height_percentage(); ?>>
-			<?php $this->bentos["bento-10-3"]->render(); ?>
+		<div id="column-11-4" class="padded column height100 width50" <?php $this->columns['column-11-4']->width_percentage(); ?>>
+			<?php $this->bentos["bento-11-3"]->render(); ?>
 		</div>
 	</div>
 </div>
