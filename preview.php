6487778ed00b39b878defcd8766811a41188b4e7
sportnak <david.m.nakayama@vanderbilt.edu>
2015-06-23 14:25:32 -0500

    annoying that I can't test from localhost with fb

--- preview.php
+++ preview.php
@@ -50,6 +50,7 @@
 	<meta property="og:url" content=<?php echo "https://www.givetoken.com/preview.php?".$token->id ?>/>
 	<meta property="og:description" content="This is dummy data"/>
 	<meta property="fb:app_id" content="1498055593756885" />
+	<meta property="og:type" content="website" />
 	<meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
 	<!-- endFavicon -->
 </head>
