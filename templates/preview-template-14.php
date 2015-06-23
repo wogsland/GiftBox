b432143fe80ec55db005ebf5190ffc778cd7777d
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-06-23 17:27:55 -0500

    Template bug fixes

---
+++ templates/preview-template-14.php
@@ -0,0 +1,18 @@
+<div class="template" id="preview">
+	<div id="column-14-1" class="column height100 width50" <?php $this->columns['column-14-1']->width_percentage(); ?>>
+		<div id="column-14-3" class="padded column height50 width100" <?php $this->columns['column-14-3']->height_percentage(); ?>>
+			<?php $this->bentos["bento-14-1"]->render(); ?>
+		</div>
+		<div id="column-14-4" class="padded column height50 width100" <?php $this->columns['column-14-4']->height_percentage(); ?>>
+			<?php $this->bentos["bento-14-2"]->render(); ?>
+		</div>
+	</div>
+	<div id="column-14-2" class="column height100 width50" <?php $this->columns['column-14-2']->width_percentage(); ?>>
+		<div id="column-14-5" class="padded column height50 width100" <?php $this->columns['column-14-5']->height_percentage(); ?>>
+			<?php $this->bentos["bento-14-3"]->render(); ?>
+		</div>
+		<div id="column-14-6" class="padded column height50 width100" <?php $this->columns['column-14-6']->height_percentage(); ?>>
+			<?php $this->bentos["bento-14-4"]->render(); ?>
+		</div>
+	</div>
+</div>
---
+++ templates/preview-template-15.php
@@ -0,0 +1,16 @@
+<div class="template" id="preview">
+	<div id="column-15-1" class="padded column height33 width100" <?php $this->columns['column-15-1']->height_percentage(); ?>>
+		<?php $this->bentos["bento-15-1"]->render(); ?>
+	</div>
+	<div id="column-15-2" class="padded column height33 width100" <?php $this->columns['column-15-2']->height_percentage(); ?>>
+		<?php $this->bentos["bento-15-2"]->render(); ?>
+	</div>
+	<div id="column-15-3" class="column height33 width100" <?php $this->columns['column-15-3']->height_percentage(); ?>>
+		<div id="column-15-4" class="padded column height100 width50" <?php $this->columns['column-15-4']->width_percentage(); ?>>
+			<?php $this->bentos["bento-15-3"]->render(); ?>
+		</div>
+		<div id="column-15-5" class="padded column height100 width50" <?php $this->columns['column-15-5']->width_percentage(); ?>>
+			<?php $this->bentos["bento-15-4"]->render(); ?>
+		</div>
+	</div>
+</div>
