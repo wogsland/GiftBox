f6a04a60c4a3d1fa70c0108be8fcbea320ee25bc
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-06-22 21:13:24 -0500

    18 and 19 to do

---
+++ templates/preview-template-19.php
@@ -0,0 +1,14 @@
+<div class="template" id="preview">
+	<div id="column-19-1" class="padded column height25 width100" <?php $this->columns['column-19-1']->height_percentage(); ?>>
+		<?php $this->bentos["bento-19-1"]->render(); ?>
+	</div>
+	<div id="column-19-2" class="padded column height25 width100" <?php $this->columns['column-19-2']->height_percentage(); ?>>
+		<?php $this->bentos["bento-19-2"]->render(); ?>
+	</div>
+	<div id="column-19-3" class="padded column height25 width100" <?php $this->columns['column-19-3']->height_percentage(); ?>>
+		<?php $this->bentos["bento-19-3"]->render(); ?>
+	</div>
+	<div id="column-19-4" class="padded column height25 width100" <?php $this->columns['column-19-4']->height_percentage(); ?>>
+		<?php $this->bentos["bento-19-4"]->render(); ?>
+	</div>
+</div>
