8d5c3b8ae8802fdf57478971f74d1f81fe6c95fd
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-06-22 22:04:56 -0500

    finished, 18, 19 to debug

---
+++ templates/preview-template-24.php
@@ -0,0 +1,18 @@
+<div class="template" id="preview">
+	<div id="column-24-1" class="column height50 width100" <?php $this->columns['column-24-1']->height_percentage(); ?>>
+		<div id="column-24-3" class="padded column height100 width50" <?php $this->columns['column-24-3']->width_percentage(); ?>>
+			<?php $this->bentos["bento-24-1"]->render(); ?>
+		</div>
+		<div id="column-24-4" class="padded column height100 width50" <?php $this->columns['column-24-4']->width_percentage(); ?>>
+			<?php $this->bentos["bento-24-2"]->render(); ?>
+		</div>
+	</div>
+	<div id="column-24-2" class="column height50 width100" <?php $this->columns['column-24-2']->height_percentage(); ?>>
+		<div id="column-24-5" class="padded column height100 width50" <?php $this->columns['column-24-5']->width_percentage(); ?>>
+			<?php $this->bentos["bento-24-3"]->render(); ?>
+		</div>
+		<div id="column-24-6" class="padded column height100 width50" <?php $this->columns['column-24-6']->width_percentage(); ?>>
+			<?php $this->bentos["bento-24-4"]->render(); ?>
+		</div>
+	</div>
+</div>
