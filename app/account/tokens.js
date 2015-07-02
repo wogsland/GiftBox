7b2a89c21dc8e6a3748603bbcc163c2edd12cee0
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-07-02 13:14:21 -0700

    edit button removed

--- app/account/tokens.js
+++ app/account/tokens.js
@@ -10,8 +10,7 @@ var AccountTokens = React.createClass({
   renderToken: function(token) {
     return <div className="col-md-3 col-xs-6 text-center" key={token.id}>
       <div className="edit-btn-group">
-      <div className="row"><button className="btn btn-xs btn-bordered edit-btn disabled"><span className="glyphicon glyphicon-pencil"></span></button></div>
-      <div className="row"><button className="btn btn-xs btn-bordered edit-btn" onClick={function() 
+      <div className="row token-row"><button className="btn btn-xs btn-bordered edit-btn" onClick={function() 
         { return token.id; }}><span className="glyphicon glyphicon-trash"></span></button></div>
       </div>
       <img className="token-edit-thumb" href='token_analytics.php' onClick = {function(){window.open('preview.php?id=' + token.id)}} src={'assets/img/token-varied-' + token.variant + '.png'} alt={token.name} width="78" /><br/>
