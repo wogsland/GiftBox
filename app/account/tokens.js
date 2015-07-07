1f1bf067758f455f69928c2a0b8edd2515663a80
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-07-06 18:54:43 -0700

    delete functionality

--- app/account/tokens.js
+++ app/account/tokens.js
@@ -10,8 +10,7 @@ var AccountTokens = React.createClass({
   renderToken: function(token) {
     return <div className="col-md-3 col-xs-6 text-center" key={token.id}>
       <div className="edit-btn-group">
-      <div className="row token-row"><button className="btn btn-xs btn-bordered edit-btn" onClick={function() 
-        { return token.id; }}><span className="glyphicon glyphicon-trash"></span></button></div>
+      <div className="row token-row"><button className="btn btn-xs btn-bordered edit-btn" href='profile.php' onClick={function() {Model.deleteToken(token.id); window.location.reload();}}><span className="glyphicon glyphicon-trash"></span></button></div>
       </div>
       <img className="token-edit-thumb" href='token_analytics.php' onClick = {function(){window.open('preview.php?id=' + token.id)}} src={'assets/img/token-varied-' + token.variant + '.png'} alt={token.name} width="78" /><br/>
       <div className="main-color"><a onClick = {function(){window.open('preview.php?id=' + token.id)}}>{token.name}</a></div>
