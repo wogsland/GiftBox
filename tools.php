289d2c76536938d6b3bf44599f451fd15955052a
John Hall <john_hall@corridor-inc.com>
2015-07-11 17:58:05 -0400

    Fixed tools.php

--- tools.php
+++ tools.php
@@ -1,10 +1,6 @@
 <?php
 include_once "util.php";
 include_once "config.php";
-use google\appengine\api\cloud_storage\CloudStorageTools;
-if ($google_app_engine) {
-	include_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
-}
 
 $tokens = execute_query("SELECT giftbox.id, name, first_name, last_name  FROM giftbox, user where giftbox.user_id = user.id ORDER BY id")->fetch_all(MYSQLI_ASSOC);
 
@@ -15,26 +11,16 @@ foreach ($tokens as $token) {
 		echo "{$bento['css_id']}: ";
 		if ($bento['image_file_name']) {
 			echo $bento['image_file_name'];
-			if ($google_app_engine) {
-				$image_path = CloudStorageTools::getPublicUrl($file_storage_path.$bento['image_file_name'], $use_https);
-			} else {
-				$image_path = $file_storage_path.$bento['image_file_name'];
-			}
-			if (file_exists($image_path)) {
-				echo " (".filesize($image_path)." bytes)";
+			if (file_exists($file_storage_path.$bento['image_file_name'])) {
+				echo " (".filesize($file_storage_path.$bento['image_file_name'])." bytes)";
 			} else {
 				echo " (FILE NOT FOUND)";
 			}
 		}
 		if ($bento['cropped_image_file_name']) {
 			echo ", {$bento['cropped_image_file_name']}";
-			if ($google_app_engine) {
-				$image_path = CloudStorageTools::getPublicUrl($file_storage_path.$bento['cropped_image_file_name'], $use_https);
-			} else {
-				$image_path = $file_storage_path.$bento['cropped_image_file_name'];
-			}
-			if (file_exists($image_path)) {
-				echo " (".filesize($image_path)." bytes)";
+			if (file_exists($file_storage_path.$bento['cropped_image_file_name'])) {
+				echo " (".filesize($file_storage_path.$bento['image_file_name'])." bytes)";
 			} else {
 				echo " (FILE NOT FOUND)";
 			}
