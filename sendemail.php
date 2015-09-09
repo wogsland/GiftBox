b85b08100ecb124a3f7387963b8eeb5aaa6c392d
John Hall <john_hall@corridor-inc.com>
2015-09-09 15:22:03 -0400

    Send message fix

--- sendemail.php
+++ sendemail.php
@@ -1,11 +1,8 @@
 <?php
 include_once 'mail.php';
-//For Testing
-//sendMail( "founder@givetoken.com", "test email", "does this work?", "test@example.com" );
 
-//Part to be added in once mail function is working again
-//$from = $_POST_['name'] . ' <' . $_POST['email'] . '>';
-//sendMail( "founder@givetoken.com", $_POST['subject'], $_POST['message'], $from);
+$from = $_POST['name'] . ' <' . $_POST['email'] . '>';
+sendMail( "founder@givetoken", $_POST['subject'], $_POST['message'], $from);
 
 //This part is the response back to the AJAX object
 // need to tell the ajax request that i am responding back with JSON
