35fb848e302ebe8aebfe0da2922e9839a5d382cd
Bradley Wogsland <bradley@wogsland.org>
2015-10-30 16:28:04 -0500

    finished build script

--- build.sh
+++ build.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # simple shell script to build project
 
+if [ "$#" -gt 0 -a "$1" = "-h" ]
+then
+  echo ""
+  echo "NAME"
+  echo "     build -- builds GiveToken project"
+  echo ""
+  echo "SYNOPSIS"
+  echo "     ./build.sh [-h] [project]"
+  echo ""
+  echo "DESCRIPTION"
+  echo "     This tool is for building & pushing the GiveToken project. Choosing the project option master or staging"
+  echo "     will push the build to givetoken.com or t-sunlight-757.appspot.com respectively. Any other project option"
+  echo "     will result in a push to <project>-dot-t-sunlight-757.appspot.com."
+  echo ""
+  echo "     The following options are available:"
+  echo ""
+  echo "     -h     Print this help screen"
+  echo ""
+  exit
+fi
+
 # run unit tests
 ./vendor/bin/phpunit --bootstrap src/tests/autoload.php -c tests.xml
 
@@ -17,5 +38,20 @@ echo "Polybuild finished."
 # push it up to gcloud
 if [ "$#" -gt 0 ]
 then
-  gcloud preview app deploy app.yaml --project t-sunlight-757 --version $1
+  case $1 in
+    "master") gcloud preview app deploy app.yaml --project stone-timing-557 --promote ;;
+    "staging") gcloud preview app deploy app.yaml --project t-sunlight-757 --promote ;;
+    *) gcloud preview app deploy app.yaml --project t-sunlight-757 --version $1 ;;
+  esac
+#  if [ "$1" = "master"]
+#  then
+#    gcloud preview app deploy app.yaml --project stone-timing-557 --promote
+#  else
+#    if [ "$1" = "staging"]
+#    then
+#      gcloud preview app deploy app.yaml --project t-sunlight-757 --promote
+#    else
+#      gcloud preview app deploy app.yaml --project t-sunlight-757 --version $1
+#    fi
+#  fi
 fi
