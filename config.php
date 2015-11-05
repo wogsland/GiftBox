2c8199a10b68417284bf6906e2bfc7f3b0795b43
Bradley Wogsland <bradley@wogsland.org>
2015-11-05 09:28:29 -0600

    added 404 error page

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <?php
-//echo '<pre>';
-//print_r($_SERVER); die;
 $server = $_SERVER['SERVER_NAME'];
-if('givetoken.com' == strtolower($server)
-|| 'stone-timing-557.appspot.com' == strtolower($server)) {
-	header("Location: https://www.givetoken.com ", true, 301);
+if ('givetoken.com' == strtolower($server)
+    || 'stone-timing-557.appspot.com' == strtolower($server)
+) {
+    header("Location: https://www.givetoken.com ", true, 301);
 }
 
 // autoload classes
@@ -23,27 +22,27 @@ $socket = null;
 $stripe_secret_key = 'sk_test_RTcVNjcQVfYNiPCiY5O9CevV';
 $stripe_publishable_key = 'pk_test_Gawg5LhHJ934MPXlvglZrdnL';
 if (isset($_SERVER['HTTPS'])) {
-	if ($_SERVER['HTTPS'] === "on") {
-		$prefix = "https://";
-		$use_https = true;
-	}
+    if ($_SERVER['HTTPS'] === "on") {
+        $prefix = "https://";
+        $use_https = true;
+    }
 }
 if (isset($_SERVER["HTTP_X_APPENGINE_COUNTRY"])) {
-	$application_id = $_SERVER["APPLICATION_ID"];
-	$google_app_engine = true;
-	if ($application_id === "s~stone-timing-557") {
-		$file_storage_path = 'gs://tokenstorage/';
-		$socket = '/cloudsql/stone-timing-557:test';
-		$stripe_secret_key = 'sk_live_MuUj2k3WOTpvIgw8oIHaON2X';
-		$stripe_publishable_key = 'pk_live_AbtrrWvSZZCvLYdiLohHC2Kz';
-	} elseif ($application_id === "s~t-sunlight-757") {
-		$file_storage_path = 'gs://tokenstorage-staging/';
-		$socket = '/cloudsql/t-sunlight-757:staging';
-	} else {
-		$file_storage_path = 'gs://tokenstorage/';
-	}
+    $application_id = $_SERVER["APPLICATION_ID"];
+    $google_app_engine = true;
+    if ($application_id === "s~stone-timing-557") {
+        $file_storage_path = 'gs://tokenstorage/';
+        $socket = '/cloudsql/stone-timing-557:test';
+        $stripe_secret_key = 'sk_live_MuUj2k3WOTpvIgw8oIHaON2X';
+        $stripe_publishable_key = 'pk_live_AbtrrWvSZZCvLYdiLohHC2Kz';
+    } elseif ($application_id === "s~t-sunlight-757") {
+        $file_storage_path = 'gs://tokenstorage-staging/';
+        $socket = '/cloudsql/t-sunlight-757:staging';
+    } else {
+        $file_storage_path = 'gs://tokenstorage/';
+    }
 } else {
-	$file_storage_path = 'uploads/';
+    $file_storage_path = 'uploads/';
 }
 $database = "giftbox";
 $user = "giftbox";
