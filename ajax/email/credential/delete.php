5368fe2d2b9bcdfd6a39f40e26ddc8a5f499649f
John Hall <john_hall@corridor-inc.com>
2015-11-30 11:57:42 -0500

    Added isset to fix undefined index error.

--- ajax/email/credential/delete.php
+++ ajax/email/credential/delete.php
@@ -4,7 +4,7 @@ use \GiveToken\EmailCredential;
 $success = 'false';
 $data = '';
 if (isset($_SESSION['user_id'])) {
-    $id = (int) $_POST['id'];
+    $id = (int) isset($_POST['id']);
     if ($id > 0) {
         $EmailCredential = new EmailCredential($id);
         $deleted = $EmailCredential->delete();
