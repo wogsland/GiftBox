484102cd8aff40193d72d2796b61af0e016f2bb9
Bradley Wogsland <bradley@wogsland.org>
2015-12-07 10:05:07 -0600

    #571

---
+++ upgrade.php
@@ -0,0 +1,59 @@
+<?php
+use \GiveToken\User;
+
+require_once __DIR__.'/config.php';
+
+if (!logged_in()) {
+    header('Location: '.$app_url.'pricing');
+}
+
+define('TITLE', 'GiveToken.com - Give a Token of Appreciation');
+require __DIR__.'/header.php';
+
+?>
+<style>
+#upgrade-button {
+  margin-top:200px;
+  margin-bottom:300px;
+}
+</style>
+</head>
+
+<body id="upgrade-page">
+  <!-- =========================
+       PRE LOADER
+  ============================== -->
+  <div class="preloader">
+    <div class="status">&nbsp;</div>
+  </div>
+
+  <!-- =========================
+       HEADER
+  ============================== -->
+  <header class="header" data-stellar-background-ratio="0.5" id="account-profile">
+    <!-- SOLID COLOR BG -->
+    <div class="">
+        <?php require __DIR__.'/navbar.php';?>
+    </div>
+    <!-- /END COLOR OVERLAY -->
+  </header>
+  <!-- /END HEADER -->
+
+  <button type="button"
+    id="upgrade-button"
+    class="btn btn-success"
+    onclick="payWithStripe('<?php echo $_SESSION['email'];?>','UPGRADE')">
+    Upgrade <i class="fa fa-chevron-right"></i>
+  </button>
+
+  <?php require __DIR__.'/footer.php';?>
+  <!-- =========================
+       PAGE SPECIFIC SCRIPTS
+  ============================== -->
+  <script src="js/pricing.min.js?v=<?php echo VERSION;?>"></script>
+  <script>
+    $('#upgrade-button').click();
+  </script>
+
+</body>
+</html>
