942f5ecab0f0255da5c834dd5e371fe851091d97
Bradley Wogsland <bradley@wogsland.org>
2015-12-08 11:18:19 -0600

    added public dir for #602 native android browser

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+//print_r($_SERVER);
+//echo $_SERVER['REQUEST_URI'];
+
+// Parse URI
+$pieces = explode('?', $_SERVER['REQUEST_URI']);
+$endpoint = $pieces[0];
+$endpoint_parts = explode('/', $endpoint);
+if (isset($pieces[1])) {
+    $gets = $pieces[1];
+    $gets = explode('&', $gets);
+    $get_parts = array();
+    foreach ($gets as $get) {
+        $parts = explode('=', $get);
+        $get_parts[$parts[0]] = $parts[1];
+    }
+}
+
+
+if ('token' == $endpoint_parts[1] && 'recruiting' == $endpoint_parts[2]) {
+    // don't display in native android browser
+    $detect = new Mobile_Detect;
+    if ($detect->isMobile()) {
+        //echo '<pre>';print_r($detect);die;
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'AppleWebKit') !== false
+        && strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') === false) {
+            include __DIR__.'/../get_chrome.html';
+            die;
+        }
+    }
+    include __DIR__.'/../recruiting_token.build.html';
+}
+?>
