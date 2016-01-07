1a4ba286002da9d463ef0910518b51c7f4ca18ca
Bradley Wogsland <bradley@wogsland.org>
2016-01-07 10:24:19 -0600

    added save ajax endpoints for company images & videos (#578)

---
+++ ajax/recruiting_company_image/save.php
@@ -0,0 +1,31 @@
+<?php
+use GiveToken\RecruitingCompanyImage;
+
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    if (isset($_POST['recruiting_company_id']) && isset($_POST['file_name'])) {
+        $recruiting_company_id = (int) $_POST['recruiting_company_id'];
+        $file_name = escape_string($_POST['file_name']);
+        try {
+            // Save the token image
+            $recruiting_company_image = new RecruitingCompanyImage();
+            $id = $recruiting_company_image->create($recruiting_company_id, $file_name);
+            $response['status'] = "SUCCESS";
+            $response['id'] = $recruiting_company_image->id;
+
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            $response['status'] = "ERROR";
+            $response['message'] = $e->getMessage();
+            $repsonse['object'] = $e;
+        }
+    } else {
+        $response['status'] = "ERROR";
+        $response['message'] = "Recruiting token id unavailable.";
+    }
+} else {
+    $response['status'] = "ERROR";
+    $response['message'] = "Session information unavailable.";
+}
+header('Content-Type: application/json');
+echo json_encode($response);
---
+++ ajax/recruiting_company_video/save.php
@@ -0,0 +1,49 @@
+<?php
+use GiveToken\RecruitingToken;
+use GiveToken\RecruitingCompanyVideo;
+
+if (isset($_SESSION['user_id'])) {
+    $user_id = (int) $_SESSION['user_id'];
+    if (isset($_POST['recruiting_company_id'])) {
+        $recruiting_company_id = (int) $_POST['recruiting_company_id'];
+        if (isset($_POST['source'], $_POST['source_id'])) {
+            $source = escape_string($_POST['source']);
+            $source_id = escape_string($_POST['source_id']);
+            if (in_array($source, ['youtube','vimeo'])) {
+                // see if company belongs to this user
+                $RecruitingCompany = new RecruitingCompany($recruiting_company_id, 'id');
+                if ($RecruitingCompany->user_id == $user_id) {
+                    try {
+                        // Save the token video
+                        $recruiting_company_video = new RecruitingCompanyVideo();
+                        $id = $recruiting_company_video->create($recruiting_company_id, $source, $source_id);
+                        $response['status'] = "SUCCESS";
+                        $response['id'] = $recruiting_company_video->id;
+                    } catch (Exception $e) {
+                        error_log($e->getMessage());
+                        $response['status'] = "ERROR";
+                        $response['message'] = $e->getMessage();
+                        $repsonse['object'] = $e;
+                    }
+                } else {
+                    $response['status'] = "ERROR";
+                    $response['message'] = "User does not have access to this token.";
+                }
+            } else {
+                $response['status'] = "ERROR";
+                $response['message'] = "Must be a YouTube or Vimeo video.";
+            }
+        } else {
+            $response['status'] = "ERROR";
+            $response['message'] = "Source and source id required.";
+        }
+    } else {
+        $response['status'] = "ERROR";
+        $response['message'] = "Recruiting token id required.";
+    }
+} else {
+    $response['status'] = "ERROR";
+    $response['message'] = "User must be logged in.";
+}
+header('Content-Type: application/json');
+echo json_encode($response);
