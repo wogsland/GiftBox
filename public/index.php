92006a820a1b02f29c692f0d881867bbdc5f9d30
Bradley Wogsland <bradley@wogsland.org>
2016-01-18 20:23:31 -0600

    Implemented HTTP client error 418 to test #723

--- public/index.php
+++ public/index.php
@@ -153,6 +153,9 @@ if (!isset($endpoint_parts[1])) {
         case 'user':
         include __DIR__.'/../admin/user_info.php';
         break;
+        case 'teapot':
+        include __DIR__.'/../teapot.php';
+        break;
         case 'test':
         // this endpoint is just for non-production testing
         if (DEVELOPMENT) {
