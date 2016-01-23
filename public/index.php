a808de311c02bc8760c33f96a9da29b9ea56b2d1
Bradley Wogsland <bradley@wogsland.org>
2016-01-23 12:53:25 -0600

    implemented 500 error page #513 for any uncaught error/exception

--- public/index.php
+++ public/index.php
@@ -1,30 +1,35 @@
 <?php
 use Sizzle\Route;
 
-require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../config.php';
+try{
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../config.php';
 
-$apis = [
-    'api.gosizzle.local',
-    'api.gosizzle.com'
-];
-if (in_array($_SERVER['SERVER_NAME'], $apis)) {
-    include __DIR__.'/../api/route.php';
-} else {
-    // Parse URI
-    $pieces = explode('?', $_SERVER['REQUEST_URI']);
-    $endpoint = $pieces[0];
-    $endpoint_parts = explode('/', $endpoint);
-    $get_parts = array();
-    if (isset($pieces[1])) {
-        $gets = $pieces[1];
-        $gets = explode('&', $gets);
-        foreach ($gets as $get) {
-            $parts = explode('=', $get);
-            $get_parts[$parts[0]] = $parts[1] ?? null;
+    $apis = [
+        'api.gosizzle.local',
+        'api.gosizzle.com'
+    ];
+    if (in_array($_SERVER['SERVER_NAME'], $apis)) {
+        include __DIR__.'/../api/route.php';
+    } else {
+        // Parse URI
+        $pieces = explode('?', $_SERVER['REQUEST_URI']);
+        $endpoint = $pieces[0];
+        $endpoint_parts = explode('/', $endpoint);
+        $get_parts = array();
+        if (isset($pieces[1])) {
+            $gets = $pieces[1];
+            $gets = explode('&', $gets);
+            foreach ($gets as $get) {
+                $parts = explode('=', $get);
+                $get_parts[$parts[0]] = $parts[1] ?? null;
+            }
         }
-    }
 
-    $route = new Route($endpoint_parts, $get_parts);
-    $route->go();
+        $route = new Route($endpoint_parts, $get_parts);
+        $route->go();
+    }
+} catch (Throwable $thrown) {
+    http_response_code(500);
+    require_once __DIR__.'/500.html';
 }
