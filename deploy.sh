bb248fb654c1f519faf7d334ac242d464982b5cb
Bradley Wogsland <bradley@wogsland.org>
2016-02-01 14:05:19 -0600

    deployment tweaking

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # deploy under the sudo crontab on devlopment
-#cd /var/www/dev.gosizzle.io/
+cd /var/www/dev.gosizzle.io/
 BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
 if [ "$BRANCH" = "develop" ]
 then
