32fdd0fe6079f6cf0bd30e3017a8bcaab7efab03
Bradley Wogsland <bradley@wogsland.org>
2016-02-01 13:53:00 -0600

    deployment tweaking

--- minute_cron.php
+++ minute_cron.php
@@ -1,6 +1,12 @@
 <?php
 // This cron is installed on development & runs every minute
-require __DIR__.'/config.php';
+require_once __DIR__.'/config/credentials.php';
+require_once __DIR__.'/src/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/util.php';
+$mysqli = new mysqli($mysql_server, $user, $password, $database);
+new Sizzle\Connection($mysqli);
+
 
 // watch for autodeployment webhook from Github
 $result = execute_query("SELECT `branch` FROM `deploy` WHERE `needed`='Yes' AND `branch`='develop'");
