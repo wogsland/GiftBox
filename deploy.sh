bbdff36715609799c7f5f18f41297610b31ffad5
Bradley Wogsland <bradley@wogsland.org>
2016-02-01 13:44:47 -0600

    deployment tweaking

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # deploy under the sudo crontab on devlopment
-cd /var/www/dev.gosizzle.io/
-BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/')"
-if [ $BRANCH = "develop" ]
+#cd /var/www/dev.gosizzle.io/
+BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+if [ "$BRANCH" = "develop" ]
 then
   dt=$(date '+%Y%m%d%H%M%S');
   git branch $dt.dev.backup
--- minute_cron.php
+++ minute_cron.php
@@ -3,7 +3,7 @@
 require __DIR__.'/config.php';
 
 // watch for autodeployment webhook from Github
-$result = execute_query("SELECT `branch` FROM `deploy` WHERE `needed`='Yes'");
+$result = execute_query("SELECT `branch` FROM `deploy` WHERE `needed`='Yes' AND `branch`='develop'");
 if($row = $result->fetch_assoc()) {
     exec(__DIR__.'/deploy.sh');
     update("UPDATE `deploy` SET `needed`='No' WHERE `branch`='develop'");
