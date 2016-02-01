81a3daea35e2b24f7e574c8c8ff75022081a582e
Bradley Wogsland <bradley@wogsland.org>
2016-02-01 13:35:30 -0600

    deployment tweaking

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # deploy under the sudo crontab on devlopment
 cd /var/www/dev.gosizzle.io/
-BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')"
+BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/')"
 if [ $BRANCH = "develop" ]
 then
   dt=$(date '+%Y%m%d%H%M%S');
