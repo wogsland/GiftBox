c8766b5e2dd752fcd5ca1158ddb8b5c1255fae29
Bradley Wogsland <bradley@wogsland.org>
2016-02-16 09:46:39 -0600

    #789 WIP

--- ajax/email/credential/add.php
+++ ajax/email/credential/add.php
@@ -3,6 +3,7 @@ use \Sizzle\EmailCredential;
 
 $success = 'false';
 $data = '';
+$errors = [];
 if (isset($_SESSION['user_id'])) {
     $params = [
         'username',
@@ -13,17 +14,22 @@ if (isset($_SESSION['user_id'])) {
     foreach ($params as $param) {
         $$param = escape_string($_POST[$param] ?? null);
     }
-    if (!filter_var($smtp_host, FILTER_VALIDATE_IP) === false
-        && (int) $smtp_port == $smtp_port
-    ) {
+    if (!filter_var($smtp_host, FILTER_VALIDATE_IP) === false) {
         $user_id = $_SESSION['user_id'];
         $EmailCredential = new EmailCredential();
         $id = $EmailCredential->create($user_id, $username, $password, $smtp_host, $smtp_port);
         if ($id > 0) {
             $success = 'true';
             $data = array('id'=>$id);
+        } else {
+            $errors[] = 'Unable to insert email credentials';
         }
+    } else {
+        $errors[] = 'Invalid IP provided for SMTP host';
     }
+}
+if (!empty($errors)) {
+    $data['errors'] = $errors;
 }
 
 header('Content-Type: application/json');
--- email_credentials.php
+++ email_credentials.php
@@ -5,7 +5,7 @@ if (!logged_in() || !is_admin()) {
 
 $user_id = $_SESSION['user_id'] ?? '';
 
-define('TITLE', 'S!zzle - Email List');
+define('TITLE', 'S!zzle - Email Credentials');
 require __DIR__.'/header.php';
 ?>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
@@ -93,16 +93,12 @@ require __DIR__.'/header.php';
     paper-dialog#status-dialog {
         --paper-dialog-background-color: #303030;
     }
-    .company-social-site-td {
-      border:0;
-      font-size: 18px;
-    }
-    .company-social-user-td {
-      border:0;
-      padding-bottom: 20px;
-      padding-left: 5px;
+    #status-dialog {
+      height: 200px;
       width: 500px;
-      font-size: 18px;
+    }
+    #status-details {
+      color: red;
     }
 </style>
 
@@ -152,8 +148,12 @@ require __DIR__.'/header.php';
    </div>
 </div>
 
-    <paper-dialog class="recruiting-dialog" id="status-dialog">
-        <p id="status-message"></p>
+    <paper-dialog class="recruiting-dialog" id="status-dialog" modal>
+      <p id="status-message"></p>
+      <p id="status-details"></p>
+      <div class="button-container">
+        <paper-button raised onclick="dismissStatusDialog()">DISMISS</paper-button>
+      </div>
     </paper-dialog>
 
     <?php require_once __DIR__.'/footer.php';?>
@@ -162,44 +162,33 @@ require __DIR__.'/header.php';
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>
     /**
-     * Attempts to upload the email list to the ajax endpoint & closes dialog
+     * Attempts to upload the email credentials to the ajax endpoint & closes dialog
      */
     function saveCredentials() {
-      $('#email-list-errors').html('Processing...');
-      var formData = new FormData($('#email-list-upload')[0]);
       $.ajax({
         url: '/ajax/email/credential/add/',
         type: 'post',
         data: {
           username: $('#credential-email').val(),
-          password: $('#credentialpassword').val(),
+          password: $('#credential-password').val(),
           smtp_host: $('#credential-host').val(),
           smtp_port: $('#credential-port').val(),
         },
-        data: formData,
         dataType: 'json',
         success: function(data, textStatus){
           var message = data.data.message+'<br />';
           if(data.success === 'true') {
             $('#status-message').html('Email credentials successfully saved');
+            $('#status-dialog')[0].open();
           }  else {
-            alert('fail')
-            /*if (data.data.errors.length) {
-              message += '<strong>Errors:</strong><br />';
-              $.each(data.data.errors, function(index, error) {
-                message += error+'<br />';
-              })
-            }*/
+            $('#status-message').html('Failed to save email credentials');
+            $('#status-dialog')[0].open();
           }
-        },
-        //Options to tell jQuery not to process data or worry about content-type.
-        cache: false,
-        contentType: false,
-        processData: false
+        }
       }).fail(function() {
-        //$('#email-list-errors').html('Uploading email list failed.');
+        $('#status-message').html('Failed to save email credentials');
+        $('#status-dialog')[0].open();
       });
-      //$('#email-list-dialog')[0].close();
     }
 
     /**
@@ -212,25 +201,11 @@ require __DIR__.'/header.php';
     }
 
     /**
-     * Registers click on hidden file input.
-     *
-     * @param {String} identifier The jQuery identifier to click
+     * Closes the Status Dialog
      */
-    function fireHiddenFileInput(identifier) {
-       $(identifier).trigger('click');
+    function dismissStatusDialog() {
+      $('#status-dialog')[0].close();
     }
-    $( document ).ready(function() {
-      $('#select-list-file').change(function() {
-        var filename = $('#select-list-file').val().replace('C:\\fakepath\\', '');
-        console.log('filename is '+filename)
-        if ($('#select-list-file:file')[0].files[0].type !== "text/plain") {
-          $('#email-list-file label').html('<font color="red">Please choose a text file</font>');
-        } else {
-          $('#email-list-file label').html('File Name');
-          $('#email-list-file').val(filename);
-        }
-      });
-    });
     </script>
 </body>
 </html>
