03faf4e1b1f25f5ed74a4ccb7f23803557a2d161
Bradley Wogsland <bradley@wogsland.org>
2016-03-02 15:10:18 -0600

    #698 WIP

---
+++ ajax/user/get_recruiter_profile.php
@@ -0,0 +1,27 @@
+<?php
+use \Sizzle\{
+    HTML,
+    RecruitingToken
+};
+
+// collect id
+$user_id = escape_string($endpoint_parts[4] ?? '');
+
+$success = 'false';
+$data = '';
+if (((int)$user_id == $user_id && is_admin()) || (logged_in() && $user_id == $_SESSION['user_id'])) {
+    /*$token = new RecruitingToken($id, 'long_id');
+    if (isset($token->id)) {
+        $success = 'true';
+        $data = $token;
+    }*/
+    $tokens = execute_query(
+        "SELECT long_id, job_title
+         FROM recruiting_token
+         WHERE user_id = '$user_id'
+         ORDER BY job_title, long_id"
+    )->fetch_all(MYSQLI_ASSOC);
+    $data = $tokens;
+}
+header('Content-Type: application/json');
+echo json_encode(array('success'=>$success, 'data'=>$data));
