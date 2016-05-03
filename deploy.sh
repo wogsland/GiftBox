6ef5244bd9dcddb07d1ad10fc2f398242e57c51c
Bradley Wogsland <bradley@wogsland.org>
2016-05-03 13:38:01 -0500

    setting up Bacon deployment GiveToken/Bacon#3

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,24 @@
-# deploy under the sudo crontab on devlopment
-cd /var/www/dev.gosizzle.io/
-BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
-if [ "$BRANCH" = "develop" ]
+# deploy under the sudo crontab on development
+if [ "$#" -gt 0 -a "$1" = "Giftbox" ]
 then
-  dt=$(date '+%Y%m%d%H%M%S');
-  git branch $dt.dev.backup
-  git pull origin develop
+  cd /var/www/dev.gosizzle.io/
+  BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+  if [ "$BRANCH" = "develop" ]
+  then
+    dt=$(date '+%Y%m%d%H%M%S');
+    git branch $dt.dev.backup
+    git pull origin develop
+  fi
+fi
+
+if [ "$#" -gt 0 -a "$1" = "Bacon" ]
+then
+  cd /var/www/dev.gosizzle.io/src/Bacon/
+  BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+  if [ "$BRANCH" = "develop" ]
+  then
+    dt=$(date '+%Y%m%d%H%M%S');
+    git branch $dt.dev.backup
+    git pull origin develop
+  fi
 fi
