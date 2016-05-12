d815d08938741a579b041aee589a705575d8288b
Bradley Wogsland <bradley@wogsland.org>
2016-05-12 11:18:30 -0500

    brought in external dependency from https://github.com/sytelus/CryptoJS/blob/master/rollups/md5.js #1019

--- profile.php
+++ profile.php
@@ -36,7 +36,7 @@ require __DIR__.'/header.php';
      ACCOUNT PROFILE
 ============================== -->
 
-<script type="text/javascript" src="https://crypto-js.googlecode.com/svn/tags/3.0.2/build/rollups/md5.js"></script>
+<script type="text/javascript" src="/js/md5.js"></script>
 <script type="text/javascript" src="/app/models/model.js?v=<?php echo VERSION;?>"></script>
 <script type="text/jsx" src="/app/account/AccountStore.js?v=<?php echo VERSION;?>"></script>
 
