cc1ceaeb676aefabd7844520cc8748caafe1aae3
Shrey Desai <shreydesai@me.com>
2016-06-09 11:09:00 -0700

    fixes #1024 and #1029

--- token/1B/recruiting-token.js
+++ token/1B/recruiting-token.js
@@ -1,5 +1,6 @@
 var scope = document.querySelector('template[is="dom-bind"]');
 var presentedInterestPopup = false;
+var openedInterestPopup = false;
 var cities = [];
 
 scope._onTrack = function(event) {
@@ -151,22 +152,32 @@ scope._onVideosClick = function(event) {
 scope._onInterestClick0 = function (event) {
   $('.interest-dialog')[0].open();
   presentedInterestPopup = true;
+  openedInterestPopup = true;
+  disableBackButton();
 };
 scope._onInterestClick1= function (event) {
   $('.interest-dialog')[1].open();
   presentedInterestPopup = true;
+  openedInterestPopup = true;
+  disableBackButton();
 };
 scope._onInterestClick2 = function (event) {
   $('.interest-dialog')[2].open();
   presentedInterestPopup = true;
+  openedInterestPopup = true;
+  disableBackButton();
 };
 scope._onInterestClick3 = function (event) {
   $('.interest-dialog')[3].open();
   presentedInterestPopup = true;
+  openedInterestPopup = true;
+  disableBackButton();
 };
 scope._onInterestClick4 = function (event) {
   $('.interest-dialog')[4].open();
   presentedInterestPopup = true;
+  openedInterestPopup = true;
+  disableBackButton();
 };
 
 /**
@@ -176,6 +187,8 @@ scope._submitInterest = submitInterest;
 function submitInterest(event) {
   var formIndex = 0;
   var eventPath = [];
+  openedInterestPopup = false;
+  enableBackButton();
   if (event.path !== undefined) {
     // Chrome
     eventPath = event.path;
@@ -263,6 +276,8 @@ scope._applyNow = function (event) {
 scope._closeInterestDialog = function (event) {
   $('.interest-dialog').each(function (i, dialog){
     dialog.close();
+    openedInterestPopup = false;
+    enableBackButton();
   });
 };
 
@@ -280,6 +295,11 @@ scope._closeApplyDialog = function (event) {
  * Navigates back to the main page
  */
 scope._onBackClick = function(event) {
+  if (openedInterestPopup) {
+    // removes functionality of back button when
+    // the interest dialog is opened
+    return;
+  }
   $('.gt-info-video').remove();
   this.$.pages.selected = 0;
 };
@@ -300,6 +320,20 @@ $(document).ready(function(){
     }
   });
 });
+
+/**
+ * Disables BACK button
+ */
+function disableBackButton() {
+  $('.back-button-lower').addClass('mdl-button--disabled');
+}
+
+/**
+ * Enables BACK button
+ */
+function enableBackButton() {
+  $('.back-button-lower').removeClass('mdl-button--disabled');
+}
 
 /**
  * Handle loading the token data and putting it where it should go
@@ -1009,6 +1043,7 @@ function handleAjaxRecruitingTokenGetResponsedAllowed(data) {
             if (!presentedInterestPopup) {
               $('.interest-dialog').each(function (i, dialog){
                 dialog.open();
+                openedInterestPopup = true;
               });
               presentedInterestPopup = true;
             }
