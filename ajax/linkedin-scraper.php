56933ba49cbdab498b550dbdfb22989f0fc4f3bb
Shrey Desai <shreydesai@me.com>
2016-06-16 09:41:57 -0700

    wrote ajax endpoint for images

--- ajax/linkedin-scraper.php
+++ ajax/linkedin-scraper.php
@@ -10,5 +10,17 @@ if (isset($_POST['name']) && !empty($_POST['name'])) {
   $data = shell_exec($command);
 }
 
+$old_name = isset($_POST['oldName']) && !empty($_POST['oldName']);
+$new_name = isset($_POST['newName']) && !empty($_POST['newName']);
+
+if ($old_name && $new_name) {
+  $success = true;
+  $old_image = $_POST['oldName'];
+  $new_image = $_POST['newName'];
+  $param = (string)$old_image . ' ' . (string)$new_image;
+  $command = 'cd .. ; cd ajax/scraper ; sh move.sh ' . $param;
+  $data = shell_exec($command);
+}
+
 echo json_encode(array('success'=>$success, 'data'=>$data));
 ?>
