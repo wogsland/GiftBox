cf1f96cbe96784427924334031f67296aa6f28f5
Bradley Wogsland <bradley@wogsland.org>
2016-06-20 11:41:50 -0500

    added adroll code to footer

--- footer.php
+++ footer.php
@@ -100,6 +100,27 @@
 <!-- =========================
      SCRIPTS
 ============================== -->
+<script type="text/javascript">
+adroll_adv_id = "ITEJPSCUW5BL3GO5UTS44U";
+adroll_pix_id = "E6HC6OSXS5GYFLRK7K2ITW";
+/* OPTIONAL: provide email to improve user identification */
+/* adroll_email = "username@example.com"; */
+(function () {
+var _onload = function(){
+if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+var scr = document.createElement("script");
+var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+scr.setAttribute('async', 'true');
+scr.type = "text/javascript";
+scr.src = host + "/j/roundtrip.js";
+((document.getElementsByTagName('head') || [null])[0] ||
+document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+};
+if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+else {window.attachEvent('onload', _onload)}
+}());
+</script>
 <script src="/js/dist/sizzle.min.js?v=<?php echo VERSION;?>"></script>
 <?php if (!logged_in()) {
   /** TODO Move these into marketing.min build */
