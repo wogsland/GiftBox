3e67eaf7856e2fb3557ceb559fed3a60e868ed23
Bradley Wogsland <bradley@wogsland.org>
2016-06-21 12:05:46 -0500

    testing #1080 on dev

--- token/1A/recruiting-token.js
+++ token/1A/recruiting-token.js
@@ -188,7 +188,6 @@ scope._submitInterest = function (event) {
   var formIndex = 0;
   var eventPath = [];
   openedInterestPopup = false;
-  console.log("event.path is "+event.path)
   if (event.path !== undefined) {
     // Chrome
     eventPath = event.path;
@@ -204,20 +203,20 @@ scope._submitInterest = function (event) {
   } else {
     // Firefox & Safari
     var currentElem = event.target;
-    console.log("currentElem is "+currentElem)
+    console.log("currentElem is ");console.log(currentElem)
     while (currentElem) {
       eventPath.push(currentElem);
       currentElem = currentElem.parentElement;
     }
-    console.log("currentElem is "+currentElem)
-    console.log("eventPath is "+eventPath)
+    console.log("currentElem is ");console.log(currentElem)
+    console.log("eventPath is ");console.log(eventPath)
     if (eventPath.indexOf(window) === -1 && eventPath.indexOf(document) === -1) {
       eventPath.push(document);
     }
     if (eventPath.indexOf(window) === -1) {
       eventPath.push(window);
     }
-    console.log("eventPath is "+eventPath)
+    console.log("eventPath is ");console.log(eventPath)
     if (eventPath[8].localName == 'location-x-card') {
       formIndex = 1;
     } else if (eventPath[5].localName == 'image-x-card') {
@@ -228,8 +227,8 @@ scope._submitInterest = function (event) {
       formIndex = 4;
     }
   }
-  console.log("eventPath is "+eventPath)
-  console.log("formIndex is "+formIndex)
+  console.log("eventPath is ");console.log(eventPath)
+  console.log("formIndex is ");console.log(formIndex)
   console.log($('.email-paper-input')[formIndex])
   if ($('.email-paper-input')[formIndex].validate()) {
     $('.submit-interest-button').addClass('disable-clicks');
