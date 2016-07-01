26fc16288f4f1f141867f64d467498668d1ff16c
Shrey Desai <shreydesai@me.com>
2016-07-01 10:03:32 -0700

    replaced add with select button

--- create_company.php
+++ create_company.php
@@ -386,7 +386,6 @@ require __DIR__.'/header.php';
       <h2>Import company information from LinkedIn</h2>
       <paper-input id="linkedin-url" label="Enter your company's LinkedIn URL" autofocus></paper-input>
       <div class="buttons">
-        <paper-button id="linkedin-add-button" class="dialog-button" onclick="addData()">Add</paper-button>
         <paper-button id="linkedin-submit-button" class="dialog-button" onclick="processLinkedIn()">Submit</paper-button>
         <paper-button id="linkedin-cancel-button" dialog-dismiss class="dialog-button" onclick="cancelLinkedIn()">Cancel</paper-button>
       </div>
