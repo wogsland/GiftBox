dc7cd51059d46d3e499923350d6fd19bb034ee80
Bradley Wogsland <bradley@wogsland.org>
2016-07-14 12:18:52 -0500

    #1112 randomizing text

--- lp/demo_request.php
+++ lp/demo_request.php
@@ -3,12 +3,12 @@ use \Sizzle\Bacon\Database\LandingPage;
 
 // If the user hasn't been here, randomize the experience
 // Otherwise, take them to the page they saw before
-if (!isset($_SESSION['landing_page'], $_SESSION['landing_page']['script'], $_SESSION['landing_page']['id'])) {
+if (!isset($_SESSION['landing_page']['demo_request'], $_SESSION['landing_page']['demo_request']['script'], $_SESSION['landing_page']['demo_request']['id'])) {
     $LandingPage = new LandingPage(rand(7,8));
-    $_SESSION['landing_page']['script'] = $LandingPage->script;
-    $_SESSION['landing_page']['id'] = $LandingPage->id;
+    $_SESSION['landing_page']['demo_request']['script'] = $LandingPage->script;
+    $_SESSION['landing_page']['demo_request']['id'] = $LandingPage->id;
 } else {
-    $LandingPage = new LandingPage($_SESSION['landing_page']['id']);
+    $LandingPage = new LandingPage($_SESSION['landing_page']['demo_request']['id']);
 }
 $LandingPage->recordHit($_COOKIE['visitor'] ?? '');
-require __DIR__.'/'.$_SESSION['landing_page']['script'];
+require __DIR__.'/'.$_SESSION['landing_page']['demo_request']['script'];
