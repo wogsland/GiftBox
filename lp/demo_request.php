b25612946aaadec84c14758bc0ae84a21caf8e08
Bradley Wogsland <bradley@wogsland.org>
2016-07-14 11:03:04 -0500

    fixing 500 error #1111

--- lp/demo_request.php
+++ lp/demo_request.php
@@ -1,14 +1,16 @@
 <?php
 use \Sizzle\Bacon\Database\LandingPage;
 
+echo 'in demo';die;
 // If the user hasn't been here, randomize the experience
 // Otherwise, take them to the page they saw before
 if (!isset($_SESSION['landing_page'], $_SESSION['landing_page']['script'], $_SESSION['landing_page']['id'])) {
     $LandingPage = new LandingPage(rand(7,8));
+    print_r($LandingPage);die;
     $_SESSION['landing_page']['script'] = $LandingPage->script;
     $_SESSION['landing_page']['id'] = $LandingPage->id;
 } else {
     $LandingPage = new LandingPage($_SESSION['landing_page']['id']);
 }
-$LandingPage->recordHit($_COOKIE['visitor']);
+$LandingPage->recordHit($_COOKIE['visitor'] ?? '');
 require __DIR__.'/'.$_SESSION['landing_page']['script'];
