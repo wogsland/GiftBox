a71f1efcb7f69a4fa43ffd396e5e53e09584889e
Bradley Wogsland <bradley@wogsland.org>
2016-08-12 15:14:14 -0500

    OOify location functions

--- token/recruiting-token.js.php
+++ token/recruiting-token.js.php
@@ -4,7 +4,7 @@ require __DIR__.'/js/Sizzle.js';
 
 // js classes
 foreach (scandir(__DIR__.'/js') as $jsFile) {
-    if (!in_array($jsFile, ['.', '..'])) {
+    if (!in_array($jsFile, ['.', '..', 'Sizzle.js'])) {
         require __DIR__.'/js/'.$jsFile;
     }
 }
@@ -29,28 +29,7 @@ scope._onSkillsClick = Sizzle.JobDescription.skillsClick;
 scope._onValuesClick = Sizzle.JobDescription.valuesClick;
 scope._onResponsibilitiesClick = Sizzle.JobDescription.responsibilitiesClick;
 scope._onPerksClick = Sizzle.JobDescription.perksClick;
-
-scope._onLocationClick = function(event) {
-  var cityId = 0;
-  if ($('#location-section').length === 0) {
-    if ($(event.target).is('div')) {
-      cityId = $(event.target).attr('id').slice(-1) - 1;
-    } else {
-      cityId = $(event.target).parents('div').attr('id').slice(-1) - 1;
-    }
-  }
-  if (isNaN(cityId)) {
-    cityId = 0;
-  }
-  handleAjaxCityGet(cities[cityId]);
-  $('.mdl-layout__drawer').removeClass('is-visible');
-  this.$.list.sharedElements = {
-    'fade-in': event.target,
-    'fade-out': event.target
-  };
-  this.$.pages.selected = 1;
-  $('google-map').resize();
-};
+scope._onLocationClick = Sizzle.Location.click;
 
 scope._onImagesClick = function(event) {
   //$('.current-section').text('Images');
@@ -581,143 +560,6 @@ echo <<<'EOT'
 }
 
 /**
- * Handles the data return from ajax/city/get
- *
- * @param {object} the data
- */
-function handleAjaxCityGet(data) {
-  if (data.id !== undefined & data.id > 0) {
-    $('.gt-info-location').text(data.name);
-    if (Sizzle.Util.dataExists(data.population)) {
-      $('.gt-city-population').text(Sizzle.Util.numberWithCommas(data.population));
-    }
-    $('.gt-city-timezone').text(data.timezone);
-    $('.gt-city-county').text(data.county);
-    $('google-map')[0].latitude = data.latitude;
-    $('google-map')[0].longitude = data.longitude;
-    if (!setLocationButtons) {
-      $('#housing-location-button').click(function(e){
-        e.preventDefault();
-        var link = document.createElement('a');
-        link.href = 'http://www.zillow.com/homes/'+encodeURIComponent(data.name);
-        link.target = '_blank';
-        if(document.createEvent){
-          // for IE 11
-          eventMouse = document.createEvent("MouseEvent");
-          eventMouse.initMouseEvent("click",true,true,window,0,0,0,0,0,false,false,false,false,0,null);
-        } else {
-          eventMouse = new MouseEvent('click');
-        }
-        link.dispatchEvent(eventMouse);
-      });
-      $('#cost-location-button').click(function(e){
-        e.preventDefault();
-        var link = document.createElement('a');
-        //link.id = 'cost-location-link'
-        link.href = 'http://www.bestplaces.net/cost-of-living/';
-        link.target = '_blank';
-        if(document.createEvent){
-          // for IE 11
-          eventMouse = document.createEvent("MouseEvent");
-          eventMouse.initMouseEvent("click",true,true,window,0,0,0,0,0,false,false,false,false,0,null);
-        } else {
-          eventMouse = new MouseEvent('click');
-        }
-        link.dispatchEvent(eventMouse);
-        //$('#cost-location-link').click();
-      });
-      setLocationButtons = true;
-    }
-
-    //temps
-    var missingTemp = false;
-    if (Sizzle.Util.dataExists(data.temp_hi_spring)) {
-      $('.gt-city-spring-hi').text(data.temp_hi_spring);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_lo_spring)) {
-      $('.gt-city-spring-lo').text(data.temp_lo_spring);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_avg_spring)) {
-      $('.gt-city-spring-avg').text(data.temp_avg_spring);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_hi_summer)) {
-      $('.gt-city-summer-hi').text(data.temp_hi_summer);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_lo_summer)) {
-      $('.gt-city-summer-lo').text(data.temp_lo_summer);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_avg_summer)) {
-      $('.gt-city-summer-avg').text(data.temp_avg_summer);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_hi_fall)) {
-      $('.gt-city-fall-hi').text(data.temp_hi_fall);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_lo_fall)) {
-      $('.gt-city-fall-lo').text(data.temp_lo_fall);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_avg_fall)) {
-      $('.gt-city-fall-avg').text(data.temp_avg_fall);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_hi_winter)) {
-      $('.gt-city-winter-hi').text(data.temp_hi_winter);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_lo_winter)) {
-      $('.gt-city-winter-lo').text(data.temp_lo_winter);
-    } else {
-      missingTemp = true;
-    }
-    if (Sizzle.Util.dataExists(data.temp_avg_winter)) {
-      $('.gt-city-winter-avg').text(data.temp_avg_winter);
-    } else {
-      missingTemp = true;
-    }
-    if (missingTemp) {
-      $('#spring-location').remove();
-      $('#summer-location').remove();
-      $('#fall-location').remove();
-      $('#winter-location').remove();
-    }
-
-    // image(s)
-    assetHost = Sizzle.Util.getAssetHost();
-    url = '/ajax/city/get_images';
-    postData = {
-      'city_id':data.id
-    };
-    $.post(url, postData, function(imgData) {
-      if (imgData.data !== undefined && imgData.data.length > 0) {
-        image_file = imgData.data[0];
-        $('#location-back').css('background',"url('"+image_file+"') center / cover");
-        $('#location-main-image').css('background',"#ffffff url('"+image_file+"') center / cover");
-        $('#location-image-grid').css('width','100%');
-        // display 1 image
-        $('#location-main-image').css('width','100%');
-      }
-    });
-  }
-}
-
-/**
  * Handles the data return from /ajax/user/get_recruiter_profile/
  *
  * @param {object} the return
@@ -886,7 +728,7 @@ function handleAjaxRecruitingTokenGet(data) {
     $.post(url, '', function(data) {
       cities = data.data;
       if (1 == cities.length) {
-        handleAjaxCityGet(cities[0]);
+        Sizzle.Location.handleAjaxCityGet(cities[0]);
         $('#doublet-location-section').remove();
         $('#triplet-location-section').remove();
       } else if (2 == cities.length) {
