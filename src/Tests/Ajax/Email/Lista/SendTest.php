8d6952ebc88cd8351e140f348d4a5ff883fd8608
Bradley Wogsland <bradley@wogsland.org>
2016-08-15 08:45:25 -0500

    reverted breaking test changes

--- src/Tests/Ajax/Email/Lista/SendTest.php
+++ src/Tests/Ajax/Email/Lista/SendTest.php
@@ -34,46 +34,7 @@ extends \PHPUnit_Framework_TestCase
      */
     public function setUp()
     {
-//       $this->overloadedPHPMailer = \Mockery::mock('overload:\PHPMailer')->makePartial();
-//       $this->overloadedPHPMailer
-//          ->shouldReceive('send')
-//          ->andReturn(true);
-//       $this->overloadedPHPMailer
-//          ->shouldReceive('postSend')
-//          ->andReturn(true);
-//       $this->overloadedPHPMailer
-//          ->shouldReceive('preSend')
-//          ->andReturn(true);
-//        $this->assertTrue((new \PHPMailer) instanceof \Mockery\MockInterface);
-
         $this->url = TEST_URL . "/ajax/email/list/send";
-
-        $cookieAndUserId = getTestCookieAndUserId();
-        $this->cookie = $cookieAndUserId[0];
-        $this->user = new User($cookieAndUserId[1]);
-        $this->recruiting_token = new RecruitingToken();
-        $this->recruiting_token->user_id = $this->user->id;
-        $this->recruiting_token->long_id = substr(md5(microtime()), rand(0, 26), 20);
-        $this->recruiting_token->save();
-
-        //set up the test list
-        $name = 'My '.rand().'th List';
-        $this->EmailList = new EmailList();
-        $this->EmailList->create($this->user->id, $name);
-        $this->EmailListEmail = new EmailListEmail();
-        $this->test_email = rand().'@GoSizzle.io';
-        $this->EmailListEmail->create($this->EmailList->id, $this->test_email);
-
-        $username = 'user'.rand();
-        $password = 'my'.rand();
-        $smtp_host = "localhost";
-        $smtp_port = 25;
-
-        $this->smtp = new fakeSMTP;
-        $this->smtp->receive();
-        // Create credentials
-        $this->EmailCredential = new EmailCredential();
-        $this->EmailCredential->create($this->user->id, $username, $password, $smtp_host, $smtp_port);
     }
 
     /**
@@ -83,46 +44,7 @@ extends \PHPUnit_Framework_TestCase
      */
     public function testRequest()
     {
-        // need to set up testing email...
-        // cookie + post vars
-        $fields = array(
-          'token_id'=>$this->recruiting_token->long_id,
-          'email_list_id'=>$this->EmailList->id,
-          'message'=>'toast',
-          'email_credential_id'=>$this->EmailCredential->id,
-          'subject'=>''
-        );
-        $fields_string = "";
-        foreach ($fields as $key=>$value) {
-            $fields_string .= $key.'='.$value.'&';
-        }
-        $fields_string = rtrim($fields_string, '&');
-        echo "START";
-        ob_start();
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
-        curl_setopt($ch, CURLOPT_URL, $this->url);
-        $response = curl_exec($ch);
-        $this->assertEquals(true, $response);
-        $json = ob_get_contents();
-        $return = json_decode($json);
-        ob_end_clean();
-        echo "GOGO";
-        var_export($this->hp);
-        if (!$this->hp->mail['rawEmail']) {
-            echo "NO EMAIL";
-        } else {
-            var_export($this->hp);
-            echo ' \n ';
-        }
-        echo 'Response Data: ';
-        var_export($json);
-//        $this->assertFalse(isset($return->data->errors));
-//        $this->assertEquals('', $return->data);
-//        $this->assertEquals('true', $return->success);
-//        $this->assertFalse(true);
+        // need to set up testing email?
     }
 
     /**
@@ -185,7 +107,7 @@ extends \PHPUnit_Framework_TestCase
         ob_start();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
+        curl_setopt($ch, CURLOPT_COOKIE, TEST_COOKIE);
         curl_setopt($ch, CURLOPT_URL, $this->url);
         $response = curl_exec($ch);
         $this->assertEquals(true, $response);
@@ -219,7 +141,7 @@ extends \PHPUnit_Framework_TestCase
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
+        curl_setopt($ch, CURLOPT_COOKIE, TEST_COOKIE);
         curl_setopt($ch, CURLOPT_URL, $this->url);
         $response = curl_exec($ch);
         $this->assertEquals(true, $response);
--- src/Tests/autoload.php
+++ src/Tests/autoload.php
@@ -16,43 +16,6 @@ new Sizzle\Bacon\Connection();
 require_once __DIR__.'/../../util.php';
 
 /**
- * This is a testing function for obtaining a valid cookie and the associated
- * user id.
- *
- * @param boolean $admin - optionally specify user is to be an admin
- *
- * @return array(string - a cookie to access a logged in session,
- *               int - the id of the associated user)
- */
-function getTestCookieAndUserId($admin = false)
-{
-    $password = 'nachos';
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-    $username = rand().'@gosizzle.io';
-    if ($admin) {
-        $query = "INSERT INTO user (first_name, last_name, email_address, password, admin)
-              VALUES ('fake', 'user', '$username', '$hash', 'Y')";
-    } else {
-        $query = "INSERT INTO user (first_name, last_name, email_address, password)
-              VALUES ('fake', 'user', '$username', '$hash')";
-    }
-    execute_query($query);
-
-    $user = (new Sizzle\Bacon\Database\User())->fetch($username);
-
-    $ch = curl_init(TEST_URL . '/ajax/login');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "login_email=$username&password=$password&login_type=EMAIL");
-    curl_setopt($ch, CURLOPT_HEADER, 1);
-    $result = curl_exec($ch);
-    preg_match_all('/^Set-Cookie: PHPSESSID=(.*?);/mi', $result, $matches);
-    $userCookie = $matches[1][0];
-    $cookieString = 'PHPSESSID=' . $userCookie . ';';
-    return array($cookieString, $user->id);
-}
-
-/**
  * This is a testing function for obtaining a valid cookie.
  *
  * @param boolean $admin - optionally specify user is to be an admin
@@ -66,10 +29,10 @@ function getTestCookie($admin = false)
     $username = rand().'@gosizzle.io';
     if ($admin) {
         $query = "INSERT INTO user (first_name, last_name, email_address, password, admin)
-              VALUES ('fake', 'user', '$username', '$hash', 'Y')";
+                  VALUES ('fake', 'user', '$username', '$hash', 'Y')";
     } else {
         $query = "INSERT INTO user (first_name, last_name, email_address, password)
-              VALUES ('fake', 'user', '$username', '$hash')";
+                  VALUES ('fake', 'user', '$username', '$hash')";
     }
     execute_query($query);
     $ch = curl_init(TEST_URL . '/ajax/login');
@@ -80,8 +43,7 @@ function getTestCookie($admin = false)
     $result = curl_exec($ch);
     preg_match_all('/^Set-Cookie: PHPSESSID=(.*?);/mi', $result, $matches);
     $userCookie = $matches[1][0];
-    $cookieString = 'PHPSESSID=' . $userCookie . ';';
-    return $cookieString;
+    return 'PHPSESSID=' . $userCookie . ';';
 }
 define('TEST_COOKIE', getTestCookie());
 if (!defined('FILE_STORAGE_PATH')) {
